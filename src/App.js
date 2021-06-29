bf049a7fac40a96b76fe9109ff5ab0b80b13a592
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-06-29 23:12:54 +0000

    app js update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import React_ass from './React_assform';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <React_ass />
+      </div>
+    );
+  }
 }
 
 export default App;
